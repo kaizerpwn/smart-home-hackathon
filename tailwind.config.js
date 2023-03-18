80add57f207062049c21016e721f8bd75dd17232
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-03-18 08:51:33 +0100

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primaryColor: "#A78BFA",
+        secondaryColor: "#7d54f8",
+        whiteSoft: "#F7EBFF",
+        greenSoft: "#005243"
+      },
+    },
+  },
+  plugins: [],
+}
