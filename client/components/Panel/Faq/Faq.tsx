be7a7ed0270730e8bcf40fc0e19461e726dc6923
hdzcalmir@gmail.com <81625008+hdzcalmir@users.noreply.github.com>
2023-03-19 02:03:05 +0100

    panel

--- client/components/Panel/Faq/Faq.tsx
+++ client/components/Panel/Faq/Faq.tsx
@@ -1,5 +1,6 @@
-
 const Faq = () => {
+
+
   return (
     <section className="bg-white dark:bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600 rounded-3xl">
   <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-16 lg:px-6">
@@ -9,71 +10,64 @@ const Faq = () => {
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What do you mean by 
+                      1. Sta su pametni uređaji?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.</p>
+                  <p className="text-gray-500 dark:text-gray-400">Pametni uređaji su uređaji koji imaju mogućnost da se spoje na internet odnosno lokalni wi-fi i nakon toga njima se može manupulisati pomoću mobilne ili web aplikacije.
+                    </p>
               </div>
               <div className="mb-10">                        
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What does exactly mean?
+                      2. Šta pametni uređaji rade?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">Once you have purchased either the design, code, or both packages, you will have access to all of the future updates based on the roadmap, free of charge.</p>
+                  <p className="text-gray-500 dark:text-gray-400">Neke od najčešćih situacija korštenja pametnih uređaja jesu paljenje i gašenje svijetla, maština za veš i posuđe, sigurnosne kamere i alarmi itd.
+                </p>
               </div>
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      How does support work?
+                      3. Da li mi je potreban Smart Home Hub s Google Homeom?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">re aware of the importance of well qualified support, that is why we decided that support will only be provided by the authors that actually worked on this project.</p>
-                  <p className="text-gray-500 dark:text-gray-400">Feel free to <a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">contact us</a> and well help you out as soon as we can.</p>
+                  <p className="text-gray-500 dark:text-gray-400">Ne, google home je dodatni uređaj koji možete dodati svom Smart Homeu i možete kontrolirati svoje pametne uređaje koristeći samo svoj glas, nije uključeno čvorište osim mobilne aplikacije pametnog uređaja.</p>
               </div>
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      I want to build more than one project. Is that allowed?
+                      4. Kako Smart Home može uštedjeti energiju?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">You can use Windster for an unlimited amount of projects, whether s a personal website, a SaaS app, or a website for a client. As long as you dont build a product that will directly compete with Windster either as a UI kit, theme, or template, its fine.</p>
-                  <p className="text-gray-500 dark:text-gray-400">Find out more information by <a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">reading the license</a>.</p>
-              </div>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Obično možete mjeriti potrošnju energije putem mobilne aplikacije, na ovaj način možete vidjeti da li jedan uređaj ili svjetlo troši više od predviđenog i programirati događaje poput uključivanja ili isključivanja čim se uređaj ne koristi za smanjenje računa za struju.</p>
+                                </div>
           </div>
           <div>
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What does  include?
+                      5.Kako izgraditi Smart Home Sistem?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">The free updates that will be provided is based on the <a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">roadmap</a> that we have laid out for this project. It is also possible that we will provide extra updates outside of the roadmap as well.</p>
+                  <p className="text-gray-500 dark:text-gray-400">Da biste izgradili sistem pametne kuće, nisu vam potrebne stotine ili čak desetine pametnih uređaja, minimalno preporučena elementa su najmanje 4, a to mogu biti: pametni utikač, pametni prekidač svjetla, pametna sijalica i pametno uključivanje - prekidač za isključivanje. Količine će ovisiti o tome koliko soba, odnosno uređaja želite kontrolirati s njima.</p>
               </div>
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What does the free version include?
+                      6.Kako da instaliram pametne uređaje?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">The <a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">free version</a> of Windster includes a minimal style guidelines, component variants, and a dashboard page with the mobile version alongside it.</p>
-                  <p className="text-gray-500 dark:text-gray-400">You can use this version for any purposes, because it is open-source under the MIT license.</p>
+                  <p className="text-gray-500 dark:text-gray-400">Zavisi od uređaja, ali su dizajnirani da budu jednostavni za instalaciju i rukovanje. Naravno, nisu svi pametni proizvodi tako jednostavni. Za neke, poput pametnog grijanja i pametnih kamera, možda će vam trebati profesionalna instalacija.
+                    </p>
               </div>
               <div className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                       <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What is the difference between Windster and Tailwind UI?
+                      7. Da li nam je potreban internet kako bi kreirali Smart Home?
                   </h3>
-                  <p className="text-gray-500 dark:text-gray-400">Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.</p>
-                  <p className="text-gray-500 dark:text-gray-400">Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.</p>
-              </div>
-              <div className="mb-10">
-                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                      <svg className="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      Can I use Windster in open-source projects?
-                  </h3>
-                  <p className="text-gray-500 dark:text-gray-400">Generally, it is accepted to use Windster in open-source projects, as long as it is not a UI library, a theme, a template, a page-builder that would be considered as an alternative to Windster itself.</p>
-                  <p className="text-gray-500 dark:text-gray-400">With that being said, feel free to use this design kit for your open-source projects.</p>
-                  <p className="text-gray-500 dark:text-gray-400">Find out more information by <a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">reading the license</a>.</p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                Neke uređaje je moguće podesiti bez interneta dok neki zahtjevaju internet kako bi bili povezani i pokrenuti kao što su pametni zvučnici i ekrani.</p>
               </div>
           </div>
       </div>
-  </div>
-</section>
+          </div>
+      </section>
+      
   )
 }
 
