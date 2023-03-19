ed945395b1697f68e0aff108029cb02683f40a48
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-03-19 03:28:42 +0100

    Stats API Fetch

--- server/controllers/analytics.js
+++ server/controllers/analytics.js
@@ -10,4 +10,20 @@ export const getRooms = (req, res) => {
         return res.status(200).json(data)
     })
 }
+
+export const getDevices = (req, res) => {
+
+    const checkUserExistsQuery = `SELECT COUNT(*) as count FROM uredjaji`;
+    db.query(checkUserExistsQuery, (err, data) => { 
+        return res.status(200).json(data)
+    })
+}
+
+export const getActiveDevices = (req, res) => {
+
+    const checkUserExistsQuery = `SELECT COUNT(*) as count FROM uredjaji WHERE status = '1'`;
+    db.query(checkUserExistsQuery, (err, data) => { 
+        return res.status(200).json(data)
+    })
+}
  
